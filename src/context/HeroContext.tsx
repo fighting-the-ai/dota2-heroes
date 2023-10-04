2deeea71da89a3447752bcc85ee6b4cd2e977d80
jvphoenix <jvitorcocal@hotmail.com>
2023-10-04 18:21:13 -0300

    Privacy config

--- src/context/HeroContext.tsx
+++ src/context/HeroContext.tsx
@@ -1,11 +1,6 @@
 import { Hero } from "@/types";
 import React, { createContext, useContext, useState, useEffect } from "react";
 
-const headers = {
-  Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJodHRwczovL3N0ZWFtY29tbXVuaXR5LmNvbS9vcGVuaWQvaWQvNzY1NjExOTgwOTg3NDg2NDEiLCJ1bmlxdWVfbmFtZSI6Imp2cGhvZW5peCIsIlN1YmplY3QiOiI4ZDNkMzA5Ni0zMmZmLTQ0N2UtOWVhMS03MjI0M2MwODEyMDYiLCJTdGVhbUlkIjoiMTM4NDgyOTEzIiwibmJmIjoxNjc1NDY0MzExLCJleHAiOjE3MDcwMDAzMTEsImlhdCI6MTY3NTQ2NDMxMSwiaXNzIjoiaHR0cHM6Ly9hcGkuc3RyYXR6LmNvbSJ9.ro69LiunoiGaWFgDk-_Wzggqi_RavJS9mwyvkQCzOtA",
-};
-
 // contexto criado
 export const HeroInfoContext = createContext<{ heroes: Hero[] | null }>({ heroes: null });
 
@@ -19,14 +14,16 @@ export function HeroInfoContextProvider(props: React.PropsWithChildren) {
   const [heroes, setHeroes] = useState<Hero[] | null>(null);
 
   useEffect(() => {
-    fetch("https://api.stratz.com/api/v1/Hero", { headers })
+    fetch("https://api.stratz.com/api/v1/Hero", {
+      headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}` },
+    })
       .then((res) => res.json())
       .then((data: Hero[]) => {
-        setHeroes(data)
+        setHeroes(data);
       })
       .catch((error) => console.error(error));
   }, []);
-  
+
   const contextValue = { heroes };
   return <HeroInfoContext.Provider value={contextValue}>{props.children}</HeroInfoContext.Provider>;
 }
